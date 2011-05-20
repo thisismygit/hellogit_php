97cce8315b574632fbf351a1da6a5f21b8414e8d
fredrick <fake@email.com>
2011-05-20 16:57:46 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Hello git!</title>
+    </head>
+    <body>
+        <?php
+        echo "Excelent of you to join us today, git.";
+        echo "<br>";
+        echo "Do you has cake?";
+        ?>
+    </body>
+</html>
